384589ae67581b6ef2645580181bcaf38a80b3dd
Lukas Bieber <bieber.jena@web.de>
2024-06-02 12:14:13 +0200

    5.5 Dimmen mit Taster

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,44 @@
 #include <Arduino.h>
 #include <math.h>
 //Variablen
+int hell=0; //Taster1
+int dunkel=1; //Taster 2
 int nulldg = 6;
 int triac = 3;
 int zuendwinkel = 10; //in Grad
-int t_s = round(pow(10,6)*zuendwinkel*0.01/180); //t_s in us
+int z_vor = 0;
 
 
 void setup() {
 pinMode(nulldg,INPUT);
 pinMode(triac,OUTPUT);
+pinMode(0,INPUT);
+pinMode(1,INPUT);
 }
 
 void loop() {
-if (digitalRead(nulldg)==1)
+if (digitalRead(hell)==1)
 {
-  delayMicroseconds(t_s);
+  zuendwinkel--;
+}
+
+if (digitalRead(dunkel)==1)
+{
+  zuendwinkel++; 
+}
+
+if (digitalRead(nulldg)==1 && z_vor==0)
+{
+  delayMicroseconds(round(pow(10,6)*zuendwinkel*0.01/180));
   digitalWrite(triac,HIGH);
   delayMicroseconds(10);
   digitalWrite(triac,LOW);
-}
-while (nulldg==1)
+  z_vor=1;
+}else if (digitalRead(nulldg)==0) //verhindert wiederholtes Gatesignal
 {
-  //warten
+  z_vor=0;
+}
 }
 
-}
+
 
